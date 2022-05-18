86ec8a33cec5773c45828cb59748646d1302c900
Rajat Kumar Singh <rajasingh@microsoft.com>
2022-05-18 14:47:48 +0530

    Barebones for manage course page

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    if (this.props.courses.length === 0) {
+      this.props.loadCourses().catch((error) => {
+        alert("Loading courses failed", error);
+      });
+    }
+
+    if (this.props.authors.length === 0) {
+      this.props.loadAuthors().catch((error) => {
+        alert("Loading authors failed", error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
